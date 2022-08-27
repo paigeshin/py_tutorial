0ac361191e8ee12f409e438dbc2ab22249bbe91c
paige shin <paigeshin1991@gmail.com>
2022-08-27 23:29:31 +0900

    random & list

---
+++ day4/nested_list.py
@@ -0,0 +1,5 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = ["1", "2", "3", "4", "5"]
+
+combined_list = [list1, list2]
+print(combined_list)
---
+++ day4/randomization.py
@@ -0,0 +1,11 @@
+import random 
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#0.0000000 - 0.999999999
+random_float = random.random()
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(love_score)
