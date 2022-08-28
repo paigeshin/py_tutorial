a541d252a541bc8aa8b6abfe77a54d10b1f0b9ea
paige shin <paigeshin1991@gmail.com>
2022-08-28 09:20:46 +0900

    loops in python

---
+++ day5/range_example.py
@@ -0,0 +1,10 @@
+for number in range(1, 11): 
+    print(number) # 1,2,3,4,5,6,7,8,9,10
+
+for number in range(1, 11, 3): 
+    print(number) # 1, 4, 7, 10 
+
+total = 0 
+for number in range(1, 101):
+    total += number 
+print(total)
